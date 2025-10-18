2ff7dc94aadb23e3f91ec15969acaa2db954b000
maria <mariamunozcruzado@gmail.com>
2025-10-18 21:49:37 +0200

    fix(backend): adjust cors to especific directions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,20 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: [
+      'http://192.168.1.31:5173',     
+      'http://ballxpitwiki.com',        
+      'http://api.ballxpitwiki.com'     
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port, '0.0.0.0');
-  console.log(`Ball x Pit API running on http://localhost:${port}`);
+  console.log(`Ball x Pit API running on http://0.0.0.0:${port}`);
 }
 
 bootstrap();
